d9d2bfab334c46bee4dfe7e13c66c439e4b88db3
AnivedKamal925 <kamalanived@gmail.com>
2025-08-16 12:52:14 +0530

    React - JSX

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React from "react";
-import ReactDOM from  "react";
+import ReactDOM from  "react-dom/client";
 
-export default function App() {
-    return (
-        <h1>Namaste React</h1>
-    );
-};
+
+const jsxHeading = <h1 className="heading">Namaste React 💻 using JSX which is HTML or XML like syntax </h1>
+
+console.log(jsxHeading)
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
