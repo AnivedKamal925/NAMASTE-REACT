fb02153dfadcf0b73b0925e9cc81ba6baf190c3e
AnivedKamal925 <kamalanived@gmail.com>
2025-08-19 23:46:07 +0530

    useState Hooks and React Fiber

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,22 @@
 import resList from "../utils/mockData";
 import RestaurantCard from "./RestaurantCard";
+import {useState} from "react";
+
 
 const Body = () => {
+
+    const [listOfRestaurants, setListOfRestaurants ] = useState(resList);
+
     return (
         <div className="body">
             <div className="search-bar">
                 <input type="text" placeholder="Search for restaurants..." />
                 <button className="search-button">Search</button>
+                <button className="filtered-restaurant" onClick={()=>{const filteredListOfRestaurant = listOfRestaurants.filter(res => res.info.avgRating > 4.2)
+                    setListOfRestaurants(filteredListOfRestaurant);
+                }}>Top Rated Restaurant</button>
             <div className="restaurant-container">
-                {resList.map((restaurant)=> <RestaurantCard key = {restaurant.info.id} resData={restaurant}/>)}
+                {listOfRestaurants.map((restaurant)=> <RestaurantCard key = {restaurant.info.id} resData={restaurant}/>)}
             </div>
             </div>
             </div>
